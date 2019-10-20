a6a4bd198e164a51563cdd33f8251a3d407eddbd
Oleksandr Geronime <geronime@gmail.com>
2019-10-21 01:47:31 +0300

    Added binding interface call declaration to thread with auto trace. First version for calls only.

--- itc.h
+++ itc.h
@@ -12,12 +12,28 @@
 
 #include "Call.h"
 #include "Timer.h"
+#include <iostream>
 
-#define DECLARE_CALL(CONNECTOR, CLASS, METHOD, ...) \
+#define DECLARE_CALL(CONNECTOR, THREAD, CLASS, METHOD, ...) \
 namespace CONNECTOR { \
-    typedef itc::Call<CLASS, ##__VA_ARGS__> Call; \
     auto METHOD = std::mem_fn(static_cast<void (CLASS::*)(__VA_ARGS__)>(&CLASS::METHOD)); \
-}
+	typedef std::tuple<__VA_ARGS__> Params; \
+	class Call : public CallBinder \
+	{ \
+	public: \
+		Call(CLASS* context, Params params) \
+			: mpCallable(new itc::Call2<CLASS, __VA_ARGS__>(context, METHOD, params)) \
+			, mThreadName(THREAD) \
+		{ \
+			std::cout << "INVOKE CALL " << #CLASS << std::endl; \
+		} \
+		const itc::_private::ICallable* getCallable() const override { return mpCallable; } \
+		const std::string& getThreadName() const override { return mThreadName; } \
+	private: \
+		const itc::_private::ICallable* mpCallable; \
+		const std::string mThreadName; \
+	}; \
+} 
 
 #define DECLARE_REQUEST(CONNECTOR, CLASS_REQUEST, METHOD_REQUEST, CLASS_RESPONSE, METHOD_RESPONSE, TYPE_RESPONSE, ...) \
 namespace CONNECTOR { \
@@ -26,8 +42,17 @@ namespace CONNECTOR { \
     auto METHOD_RESPONSE = std::mem_fn(static_cast<void (CLASS_RESPONSE::*)(TYPE_RESPONSE)>(&CLASS_RESPONSE::METHOD_RESPONSE)); \
 }
 
+
+class CallBinder {
+public:
+	virtual ~CallBinder() {};
+	virtual const itc::_private::ICallable* getCallable() const = 0;
+	virtual const std::string& getThreadName() const = 0;
+};
+
 namespace itc {
     void createEventLoop(const std::string& threadName);
+	bool invoke(const CallBinder& callBinder);
     bool invoke(const std::string& threadName, const _private::ICallable* call);
     Timer& createTimer(const std::string& threadName, const _private::ICallable* call, 
         std::chrono::milliseconds period, bool repeating);
